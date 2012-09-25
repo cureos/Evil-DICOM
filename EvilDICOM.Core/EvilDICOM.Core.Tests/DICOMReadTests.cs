6d9ec38b4bdce4aa49fa280ba4e8541cec94b922
Anders Gustafsson, Cureos AB <anders@cureos.com>
2012-09-25 13:16:41 +0200

    Added unit test project for regular library

---
+++ EvilDICOM.Core/EvilDICOM.Core.Tests/DICOMReadTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EvilDICOM.Core.IO.Reading;
+using EvilDICOM.Core.Tests.Properties;
+
+namespace EvilDICOM.Core.Tests
+{
+    [TestClass]
+    public class DICOMReadTests
+    {
+        [TestMethod]
+        public void ReadExplicityLittleEndian()
+        {
+            var dcm = DICOMFileReader.Read(Resources.explicitLittleEndian);
+            var elemCount = dcm.AllElements.Count;
+            Assert.AreEqual(elemCount, 72);
+        }
+
+        [TestMethod]
+        public void ReadImplicitLittleEndian()
+        {
+            var dcm = DICOMFileReader.Read(Resources.implicitLittleEndian);
+            var elemCount = dcm.AllElements.Count;
+            Assert.AreEqual(elemCount, 47);
+        }
+
+        [TestMethod]
+        public void ReadBigEndian()
+        {
+            var dcm = DICOMFileReader.Read(Resources.explicitBigEndian);
+            var elemCount = dcm.AllElements.Count;
+            Assert.AreEqual(elemCount, 44);
+        }
+
+        [TestMethod]
+        public void ReadJPEG()
+        {
+            var dcm = DICOMFileReader.Read(Resources.explicitLittleJPEG);
+            var elemCount = dcm.AllElements.Count;
+            Assert.AreEqual(elemCount, 80);
+        }
+
+    }
+}
