05216604a3d18646a5b3611070bb49f045f23daf
Anders Gustafsson <anders@cureos.com>
2015-03-20 12:26:40 +0100

    Removed invalid using directive

--- EvilDICOM.Core/EvilDICOM.Core/Dictionaries/TagDictionary.cs
+++ EvilDICOM.Core/EvilDICOM.Core/Dictionaries/TagDictionary.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization.Formatters;
 using EvilDICOM.Core.Element;
 using EvilDICOM.Core.Enums;
 using EvilDICOM.Core.Interfaces;
